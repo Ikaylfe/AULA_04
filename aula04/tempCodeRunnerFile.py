13ae9b120043804281fabe40d54888f48d5dba2e
Ikaylfe <kayane.brito.7@gmail.com>
2026-04-07 21:43:46 -0300

    atualizando atividade 04

--- aula04/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-valor_inicial = float (input('Insira o valor total da compra:  '))
-tipo_pgto = (input ('Forma de pgto:')) .lower ()
-if valor_inicial > 250.00 and tipo_pgto == "a vista":
-    desconto = valor_inicial * 0.16  
-    print (f'Total a pagar = {valor_inicial - desconto:.2f}.')
-else:
-    print (f'Total a pagar = {valor_inicial: .2f}')
-    
-
